0d2b1629f28dd1e6d1d0b7045991dd7f5738dc41
sainsh <kfsainsh@gmail.com>
2019-07-31 12:03:18 +0200

    added GameView, forced fullscreen, no tittle and landscape

---
+++ app/src/main/java/com/example/a2dandroidgame/GameView.java
@@ -0,0 +1,30 @@
+package com.example.a2dandroidgame;
+
+import android.content.Context;
+import android.view.SurfaceView;
+import android.view.SurfaceHolder;
+
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+
+    public GameView(Context context){
+        super(context);
+
+        getHolder().addCallback(this);
+
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+    }
+}

399fcf0636664a9b14ec24d2ed02f47c3a274574
sainsh <kfsainsh@gmail.com>
2019-07-31 12:41:19 +0200

    added GameObject

---
+++ app/src/main/java/com/example/a2dandroidgame/GameObject.java
@@ -0,0 +1,85 @@
+package com.example.a2dandroidgame;
+
+import android.graphics.Bitmap;
+
+public class GameObject {
+
+    private Bitmap image;
+
+    private final int rowCount, colCount;
+
+    private final int WIDTH, HEIGHT;
+
+    private final int width, height;
+
+    private int x, y;
+
+    public GameObject(Bitmap image, int rowCount, int colCount, int x, int y) {
+        this.image = image;
+        this.rowCount = rowCount;
+        this.colCount = colCount;
+        this.x = x;
+        this.y = y;
+
+        this.WIDTH = image.getWidth();
+        this.HEIGHT = image.getHeight();
+
+        this.width = WIDTH / colCount;
+        this.height = HEIGHT / rowCount;
+    }
+
+    //creates a smaller object from a spritesheet
+    private Bitmap createSubImageAt(int row, int col) {
+        Bitmap subImage = Bitmap.createBitmap(image, col * width, row * height, width, height);
+        return subImage;
+    }
+
+    public Bitmap getImage() {
+        return image;
+    }
+
+    public void setImage(Bitmap image) {
+        this.image = image;
+    }
+
+    public int getRowCount() {
+        return rowCount;
+    }
+
+    public int getColCount() {
+        return colCount;
+    }
+
+    public int getWIDTH() {
+        return WIDTH;
+    }
+
+    public int getHEIGHT() {
+        return HEIGHT;
+    }
+
+    public int getWidth() {
+        return width;
+    }
+
+    public int getHeight() {
+        return height;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
+

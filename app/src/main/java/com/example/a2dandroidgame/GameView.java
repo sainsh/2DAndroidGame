dec2cc0995ad8e067d8fac1ec6efe2f09dab622e
sainsh <kfsainsh@gmail.com>
2019-07-31 14:40:48 +0200

    added GameView renamed char to Character

--- app/src/main/java/com/example/a2dandroidgame/GameView.java
+++ app/src/main/java/com/example/a2dandroidgame/GameView.java
@@ -1,21 +1,43 @@
 package com.example.a2dandroidgame;
 
 import android.content.Context;
-import android.view.SurfaceView;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 
-    public GameView(Context context){
+    private GameThread gameThread;
+    private PlayerObject player;
+
+
+    public GameView(Context context) {
         super(context);
 
-        getHolder().addCallback(this);
+        //get focus to get user events
+        this.setFocusable(true);
 
+        //set callbacks
+        getHolder().addCallback(this);
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+
+        this.player.draw(canvas);
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        Bitmap playerBitmap = BitmapFactory.decodeResource(this.getResources(), R.drawable.character);
+        this.player = new PlayerObject(this, playerBitmap, 100, 50);
 
+        this.gameThread = new GameThread(this, surfaceHolder);
+        this.gameThread.setRunning(true);
+        this.gameThread.start();
     }
 
     @Override
@@ -25,10 +47,21 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 
     @Override
     public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        boolean retry = true;
+        while (retry) {
+            try{
+                this.gameThread.setRunning(false);
 
+                //app thread has to wait until gameThread is done
+                this.gameThread.join();
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+            retry = true;
+        }
     }
 
-    public void update(){
-
+    public void update() {
+        this.player.update();
     }
 }
